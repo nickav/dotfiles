bf13dfdbd01577b84989ad54b70c6c820b777b82
Nick Aversano <nick23hi@gmail.com>
2018-07-26 15:21:17 -0700

    map ctrl-[ to esc

---
+++ .hammerspoon/Spoons/map_vim_escape.lua
@@ -0,0 +1,15 @@
+local function pressFn(mods, key)
+	if key == nil then
+		key = mods
+		mods = {}
+	end
+
+	return function() hs.eventtap.keyStroke(mods, key, 1000) end
+end
+
+local function remap(mods, key, pressFn)
+	hs.hotkey.bind(mods, key, pressFn, nil, pressFn)
+end
+
+
+remap({'ctrl'}, '[', pressFn('escape'))
