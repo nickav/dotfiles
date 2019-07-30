525e294ebd286248f63ac74471dc156a63fddb1c
Nick Aversano <nick23hi@gmail.com>
2019-07-30 18:06:52 -0400

    add sleep shortcut

---
+++ .hammerspoon/Spoons/sleep.lua
@@ -0,0 +1,10 @@
+local function runCommand(cmd)
+	return function() hs.execute(cmd) end
+end
+
+local function remap(mods, key, pressFn)
+	hs.hotkey.bind(mods, key, pressFn, nil, nil)
+end
+
+-- sleep command
+remap({'alt', 'cmd'}, 'f15', runCommand("pmset sleepnow"))
--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,4 @@
 require 'Spoons/remap_control_hjkl'
 require 'Spoons/map_vim_escape'
 require 'Spoons/media'
+require 'Spoons/sleep'
