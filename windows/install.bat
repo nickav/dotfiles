243cf1fe061cebc94e12bf66fd714251172b9ed6
Nick Aversano <nick23hi@gmail.com>
2019-07-31 21:22:55 -0400

    create linux-like aliases for windows shell

--- windows/install.bat
+++ windows/install.bat
@@ -1,7 +1,16 @@
+@echo off
+
 pushd "C:\Users\%USERNAME%\Documents\"
+del AutoHotKey.ahk
 mklink AutoHotKey.ahk "C:\Users\%USERNAME%\dotfiles\windows\AutoHotKey.ahk"
 popd
 
 pushd "C:\Users\%USERNAME%\AppData\Roaming\"
+del .emacs
 mklink .emacs "C:\Users\%USERNAME%\dotfiles\.emacs"
+popd
+
+pushd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+del startup.bat
+mklink startup.bat "C:\Users\%USERNAME%\dotfiles\windows\startup.bat"
 popd
