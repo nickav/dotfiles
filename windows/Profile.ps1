a8d62e585d8e67274cae1aec60109b65be38a6ec
Nick Aversano <nick23hi@gmail.com>
2022-02-24 19:50:53 -0500

    add PowerShell Profile.ps1

---
+++ windows/Profile.ps1
@@ -0,0 +1,49 @@
+# cd C:\Users\%USERNAME%\Documents\WindowsPowerShell\ && mklink Profile.ps1 C:\dev\dotfiles\windows\Profile.ps1
+
+function mv { move @Args }
+function cp { copy @Args }
+function rm { del @Args }
+function which { where @Args }
+
+function mklink ($target, $link) {
+  New-Item -Path $link -ItemType SymbolicLink -Value $target
+}
+
+function .. { cd .. }
+function ... { cd ..\.. }
+function .... { cd ..\..\.. }
+function ..... { cd ..\..\..\.. }
+function ~ { cd ~ }
+function doc { cd ~\Documents }
+function dev { cd C:\dev }
+function dow { cd ~\Downloads }
+function dot { cd C:\dev\dotfiles }
+function dots { cd C:\dev\dotfiles }
+
+function e { exit $Args }
+function vi { vim $Args }
+
+function g { git @Args }
+function ga { git add @Args }
+function ga. { git add . @Args }
+function gam { git commit --amend @Args }
+function gau { git add -u  @Args }
+function gs { git status @Args }
+function gc { git checkout @Args }
+function gci { git commit -m @Args }
+function gcm { git checkout master @Args }
+function gcd { git checkout dev @Args }
+function gd { git diff @Args }
+function gdm { git diff master @Args }
+function gdh { git diff HEAD @Args }
+function gdh1 { git diff HEAD~1 @Args }
+function gdh2 { git diff HEAD~2 @Args }
+function gdh3 { git diff HEAD~3 @Args }
+function gdh4 { git diff HEAD~4 @Args }
+function gl { git log @Args }
+function gpu { git push -u origin $(git rev-parse --abbrev-ref HEAD) @Args }
+function gp { git push @Args }
+function gpf { git push --force-with-lease @Args }
+function gpl { git pull @Args }
+function gsh { git stash @Args }
+function gsp { git stash pop @Args }
--- windows/install.bat
+++ windows/install.bat
@@ -4,11 +4,19 @@
 ::  C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
 ::  C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\
 
+:: Run PowerShell in admin mode:
+::  set-executionpolicy remotesigned
+
 set dotfiles_path=%~dp0%
 
 pushd "C:\Users\%USERNAME%\Documents\"
   if exist AutoHotKey.ahk (del AutoHotKey.ahk)
   mklink AutoHotKey.ahk "%dotfiles_path%AutoHotKey.ahk"
+popd
+
+pushd "C:\Users\%USERNAME%\Documents\WindowsPowerShell\"
+  if exist Profile.ps1 (del Profile.ps1)
+  mklink /H Profile.ps1 "%dotfiles_path%Profile.ps1"
 popd
 
 pushd "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
